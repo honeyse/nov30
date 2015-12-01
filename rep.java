f44c844db043e1d996583bac201c563b6fdaac8a
[java_dev] <honeysebastian@gmail.com>
2015-12-01 12:47:00 -0800

    'adding method ''message"

--- rep.java
+++ rep.java
@@ -3,8 +3,12 @@ class  rep {
 void display() {
 System.out.println("Hello from git rep");
 }
+void message() {
+	System.out.println("Success");
+}
 public static void main(String [] args) {
 rep r = new rep();
 r.display();
+r.message();
 }
 }
